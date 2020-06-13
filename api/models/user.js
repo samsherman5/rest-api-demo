2ec6db0bdb429ac4c1d4ada1bcac1a0eff9062f6
Sam Sherman <samsher4606@gmail.com>
2020-06-13 15:08:48 -0400

    Test

--- api/models/user.js
+++ api/models/user.js
@@ -6,6 +6,7 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        //For email validation
         match: /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
     },
     password: {type: String, required: true}
